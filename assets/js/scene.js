3528c67b4b012e4045bb6c092ecc94becb4be470
KrishnanRGithub <vinokrish001@gmail.com>
2023-02-07 18:55:14 +0530

    shooting not working

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -23,20 +23,65 @@ var createScene = async function () {
     camera.setTarget(BABYLON.Vector3.Zero());
     camera.attachControl(canvas, true);
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-    light.intensity = 1;
-   
-
-    var box = BABYLON.MeshBuilder.CreateBox("box", {size: 1.3}, scene);
-    box.position = new BABYLON.Vector3(0, 1, 0);
-    var material = new BABYLON.StandardMaterial("material", scene);
-    material.diffuseTexture = new BABYLON.Texture("assets/img/face1.png", scene);
-    box.material = material;
-
+    light.intensity = 1;    
     const xr = await scene.createDefaultXRExperienceAsync({
         uiOptions: {
             sessionMode: 'immersive-ar'
-        }
+        },
+        optionalFeatures: true,
     });
+
+    scene.onPointerObservable.add((e) => {
+        if (e.type === BABYLON.PointerEventTypes.POINTERUP) {
+            const multiPickInfo = scene.multiPick(scene.pointerX, scene.pointerY)
+            if (multiPickInfo.length > 1) {
+                multiPickInfo.slice(1).forEach(pickInfo => {
+                    if (pickInfo.pickedMesh?.renderingGroupId === 1) {
+                        pickInfo.pickedMesh.rotate(BABYLON.Axis.Y, -0.2, BABYLON.Space.WORLD)
+                    }
+                })
+            }
+        }
+    })
 
     return scene;
 };
+
+
+window.initFunction = async function () {
+    var asyncEngineCreation = async function () {
+        try {
+            return createDefaultEngine();
+        } catch (e) {
+            console.log("the available createEngine function failed. Creating the default engine instead");
+            return createDefaultEngine();
+        }
+    }
+
+    window.engine = await asyncEngineCreation();
+    if (!engine) throw 'engine should not be null.';
+    startRenderLoop(engine, canvas);
+    window.scene = createScene();
+};
+
+var loadScene= async function(){
+    await initFunction()
+    scene.then(returnedScene => {
+        createSprite(returnedScene).then((returnedScene2)=>{
+            sceneToRender = returnedScene;
+
+            console.log("Sprite created")
+        })
+
+    });
+}
+loadScene().then(()=>{
+    console.log("Scene loaded")
+})
+
+
+// Resize
+window.addEventListener("resize", function () {
+    engine.resize();
+});
+
--- assets/js/sprite.js
+++ assets/js/sprite.js
@@ -1,9 +1,21 @@
-function createSprite(){
-    var box = BABYLON.MeshBuilder.CreateBox("box", {size: 2}, scene);
-    box.position = new BABYLON.Vector3(0, 1, 0);
+var createSprite = async function (scene) {
+    let position ={x:2,y:1,z:4}
+    var sprite = BABYLON.MeshBuilder.CreateBox("sprite"+0, {size: 1}, scene);
+    sprite.position = new BABYLON.Vector3(position.x,position.y,position.z);
     var material = new BABYLON.StandardMaterial("material", scene);
-    material.diffuseTexture = new BABYLON.Texture("assets/img/face1.png", scene);
-    box.material = material;
+    material.diffuseTexture = new BABYLON.Texture("./assets/img/face1.png", scene);
+    sprite.material = material;
+    //   Create a particle system
+    sprite.isPickable = true;
+
+    const particleSystem = new BABYLON.ParticleSystem("particles", 10);
+
+    //Texture of each particle
+    particleSystem.particleTexture = new BABYLON.Texture("./assets/img/flare.png");
+    particleSystem.emitter = sprite;
+    particleSystem.start();
+
 }
+
 
 
