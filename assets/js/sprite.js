e498d86f8a63a0b66087c44b95feb994d9d72be6
KrishnanRGithub <vinokrish001@gmail.com>
2023-02-16 16:14:02 +0530

    random shooter built

--- assets/js/sprite.js
+++ assets/js/sprite.js
@@ -1,56 +1,28 @@
-const BULLET_RESPONSE_TIME = 1000;
+const BULLET_RESPONSE_TIME = 2000;
+var GLOBAL_STATE=1;
+var SPRITE_ID=0;
+//1-Running
+//0-Pause/Stopped
+var spriteCounter = 0;
+var currentSprite = 0;
+const MAX_NUMBER_OF_SPRITES = 4;
 var spritesList={
     "spritex":{"alive":true,"position":[0,0,0],"id:":"x"},
 
 }
-var spriteCounter = 0;
-var createBullet = async function (scene,from, to) {
-    // from = new BABYLON.Vector3(from._x,from._y,from._z)
-    // to = new BABYLON.Vector3(to._x,to._y,to._z)
-    //write a babylonjs code to load a gltf model from ./assets/models/bullet/scene.gltf at 
-    //position from and set animation for it from posiion from to position to
-    console.log("createBullet")
-    // BABYLON.SceneLoader.ImportMesh("", "/assets/models/bullet/", "scene.gltf", scene, function (newMeshes) {
-    //     var bullet = newMeshes[0];
-    //     bullet.position = from;
-    //     var bulletAnimation = new BABYLON.Animation("myAnimation", "position", 30, BABYLON.Animation.ANIMATIONTYPE_VECTOR3, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
-    //     var keys = [];
-    //     keys.push({
-    //         frame: 0,
-    //         value: bullet.position
-    //     });
-    //     keys.push({
-    //         frame: 100,
-    //         value: to
-    //     });
-        
-    //     bulletAnimation.setKeys(keys);
-        
-    //     bullet.animations = [];
-    //     bullet.animations.push(bulletAnimation);
-    //     bullet.checkCollisions = true;
-    //     scene.beginAnimation(bullet, 0, 100, false);
-    //     setTimeout(() => {
-    //         bullet.dispose();
-    //     },5000)
-    // });
+var createNewPosition = function(){
+    return {x:Math.random()*10+2,y:Math.random()*10+1,z:Math.random()*10+4}
+}
 
+var createBullet = async function (scene,from, to) {
+    if(GLOBAL_STATE==0){return;}
+    
     var bulletBallMaterial = new BABYLON.StandardMaterial("bulletBallMaterial", scene);
     bulletBallMaterial.emissiveColor = new BABYLON.Color3(0, 1, 0);  // set neon blue color
     bulletBallMaterial.diffuseColor = new BABYLON.Color3(0, 1, 0);  
 
     let bullet = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.1}, scene);
-    // let bullet = await BABYLON.SceneLoader.ImportMesh("", "/assets/models/bullet/", "scene.gltf", scene)
-    // const particleSystem = new BABYLON.ParticleSystem("particles", 200);
     bullet.material = bulletBallMaterial;
-
-    // //Texture of each particle
-    // particleSystem.particleTexture = new BABYLON.Texture("./assets/img/particle.png");
-    // particleSystem.minSize = 0.3;
-    // particleSystem.maxSize = 0.6;
-    // particleSystem.emitter = bullet;
-    // particleSystem.start();
-
 
     bullet.position = from;
     console.log(to)
@@ -77,6 +49,8 @@ var createBullet = async function (scene,from, to) {
     
 }
 var createSprite = async function (scene,camera) {
+    if(GLOBAL_STATE==0){return;}
+    if(currentSprite>=MAX_NUMBER_OF_SPRITES){return}
     var spriteMaterial = new BABYLON.StandardMaterial("spriteMaterial", scene);
     spriteMaterial.diffuseTexture = new BABYLON.Texture("./assets/img/face1.png", scene);
 
@@ -85,14 +59,14 @@ var createSprite = async function (scene,camera) {
     attackBallMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);  
 
     let cameraPosition = camera.position;
-    let position ={x:2,y:1,z:4}
+    let position = createNewPosition()
     var sprite = BABYLON.MeshBuilder.CreateBox("sprite"+spriteCounter, {size: 1}, scene);
     sprite.position = new BABYLON.Vector3(position.x,position.y,position.z);
     sprite.material = spriteMaterial;
     //   Create a particle system
     sprite.isPickable = true;
 
-    const particleSystem = new BABYLON.ParticleSystem("particles", 5);
+    const particleSystem = new BABYLON.ParticleSystem("particles", 25);
     particleSystem.minSize = 0.1;
     particleSystem.maxSize = 0.2;
 
@@ -100,18 +74,22 @@ var createSprite = async function (scene,camera) {
     particleSystem.particleTexture = new BABYLON.Texture("./assets/img/particle.png");
     particleSystem.emitter = sprite;
     particleSystem.start();
+    
+    spritesList["sprite"+spriteCounter]={"alive":true,"position":position,"id:":spriteCounter}
+    spriteCounter++;
+    currentSprite++;
 
     sprite.actionManager = new BABYLON.ActionManager(scene);
 	sprite.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPickTrigger, function (evt) {
 		var targetPosition= evt.additionalData.pickedPoint
-        console.log(sprite)
         setTimeout(() => {
             sprite.dispose();
-            console.log(sprite)
+            spritesList[sprite.id]["alive"]=false;
         },BULLET_RESPONSE_TIME)
 	}));
 
     setInterval(() => {
+        if(GLOBAL_STATE==0){return;}
         let attackBall = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 0.1}, scene);
         attackBall.position = new BABYLON.Vector3(position.x, position.y, position.z);
        
@@ -144,6 +122,10 @@ var createSprite = async function (scene,camera) {
 
     
 }
-
+var loadScene = async function (scene,camera) {
+    setInterval(() => {
+        createSprite(scene,camera);
+    }, 1000);
+}
 
 
